0002000c4805f40544203a93affb67321dc27e23
FranzDuran <franzduran.fd@gmail.com>
2023-04-24 13:53:16 -0300

    config vercel2

--- api/src/db.js
+++ api/src/db.js
@@ -18,6 +18,7 @@ const sequelize = new Sequelize(`postgres://${EF_DB_USER}:${EF_DB_PASSWORD}@${EF
 //const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectModule: pg,
 });
 const basename = path.basename(__filename);
 
