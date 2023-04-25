a4e6390269b18af24eb4031110c0827b9e1601b0
FranzDuran <franzduran.fd@gmail.com>
2023-04-24 21:11:49 -0300

    front style

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,16 +1,27 @@
 import React from "react";
-import { NavLink} from "react-router-dom";
-import style from "./landingPage.module.css"
+import { NavLink } from "react-router-dom";
+import "./landingPage.css"
+import image from "./assets/pokemon.jpg"
+import poke from "./assets/pokebola.gif"
 
-const LandingPage = () =>{
-    return(
-        <React.Fragment>
-            <div className={style.container}>
-            <h1 className={style.title}>Polemon App</h1>
-            <NavLink to={"/home"}><div className={style.button}><button className={style.btn}>Start</button></div></NavLink>
+const LandingPage = () => {
+    return (
+        <>
+        <div className="container">
+            <div className="container-info">
+                <p >Welcome to the</p>
+                <h3 >POKEMON APP</h3>
+                <NavLink to={"/home"} className="button">Lest´s go!</NavLink>
             </div>
-            
-        </React.Fragment>        
+            <img src={image} alt="Pokemon" className="container-img" />
+        </div>
+        <div className="container-responsive">
+        <p className="parrafo">Welcome to the</p>
+        <h3 className="tittle">POKEMON APP</h3>
+        <NavLink to={"/home"} className="button">Lest´s go!</NavLink>
+        <img src={poke} alt="Pokemon" className="img" />
+    </div></>
+        
     )
 }
 export default LandingPage;
