afa2eb725f437a49396b9b77f4bde02b7784ba1e
FranzDuran <franzduran.fd@gmail.com>
2023-04-24 23:23:27 -0300

    url back

--- api/src/app.js
+++ api/src/app.js
@@ -9,7 +9,8 @@ require('./db.js');
 const server = express();
 
 //const HOST='http://localhost:3000'
-const HOST="https://thunderous-khapse-bab9d4.netlify.app"  //---URL DEPLOY FRONT
+//const HOST="https://thunderous-khapse-bab9d4.netlify.app"  //---URL DEPLOY FRONT
+const HOST="https://proyecto-pokemon-api.vercel.app"
 
 server.name = 'API';
 
